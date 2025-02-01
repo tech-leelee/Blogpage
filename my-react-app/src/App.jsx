2f3cae6a6f949a4a4bf356680b01f61213076db0
Leonie.WMc <121554164+tech-leelee@users.noreply.github.com>
2025-02-01 20:19:04 +0100

    blogpage react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+
+
+function App() {
+  return (
+       <div className="card">
+           <img src="src/assets/illustration-article.svg" alt="article-image" className="articleimg"/>
+           <div className="tab">
+               <p>Learning</p>
+           </div>
+           
+           <div className="text">
+            <p>Published 21 Dec 2023</p>
+            <h3>HTML & CSS foundations</h3>
+            <p className="txt1">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+          </div>
+
+           <div className="footer">
+              <img src="src/assets/image-avatar.webp" alt="Greg" className="gregimg" />
+              <div><p>Greg Hooper</p></div>       
+          </div>
+    
+        </div>
+     )     
+}
+
+export default App
+
+
+// 
